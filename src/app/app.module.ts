27f27fbd9363667c91b5e6aa16dab77d8fb907f9
William Chan <you@example.com>
2020-04-23 01:14:08 -0400

    add in modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { ProductOverviewComponent } from './product-overview/product-overview.component';
-import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductReviewComponent } from './product-review/product-review.component';
-import { FooterComponent } from './footer/footer.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { MenuComponent } from "./menu/menu.component";
+import { ProductDescriptionComponent } from "./product-description/product-description.component";
+import { ProductOverviewComponent } from "./product-overview/product-overview.component";
+import { ProductReviewComponent } from "./product-review/product-review.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,13 +17,10 @@ import { FooterComponent } from './footer/footer.component';
     ProductOverviewComponent,
     ProductDescriptionComponent,
     ProductReviewComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, HttpClientModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/product-review/product-review.component.ts
+++ src/app/product-review/product-review.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Review } from '../contracts';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-product-review',
@@ -9,9 +10,28 @@ import { Review } from '../contracts';
 export class ProductReviewComponent implements OnInit {
   @Input() reviews: Review[];
   
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  closeResult: string;
 
   ngOnInit(): void {
   }
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
